f9cfd5229b8e53952223a802a6db21bb7ac1ee96
Markus Hubig <markus.hubig@dm.de>
2018-09-13 00:24:46 +0200

    crumbs 0.0.2

--- Formula/crumbs.rb
+++ Formula/crumbs.rb
@@ -1,13 +1,16 @@
 class Crumbs < Formula
   desc "Command-line bookmarking system for sh like consoles"
   homepage "https://github.com/fasseg/crumbs"
-  url "https://github.com/fasseg/crumbs/archive/0.0.1.tar.gz"
-  sha256 "5a799dbde93b7eb51050380c46a96e08547169b2642689b0a921bff687bc80fc"
+  url "https://github.com/fasseg/crumbs/archive/0.0.2.tar.gz"
+  sha256 "6e1d314ef3898bc92b64aa2bffb1b24b7011b07914a0e95617227f32a1450547"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+
+    bash_completion.install "crumbs-completion.bash"
+    fish_completion.install "crumbs-completion.fish"
   end
 
   test do
