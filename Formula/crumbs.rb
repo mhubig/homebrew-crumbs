55a3b95215860cebbbcae54d4a2fd51e6b6ad3ea
Markus Hubig <markus.hubig@dm.de>
2018-09-13 19:56:03 +0200

    Adds the findutils as dependecy.

--- Formula/crumbs.rb
+++ Formula/crumbs.rb
@@ -4,6 +4,8 @@ class Crumbs < Formula
   url "https://github.com/fasseg/crumbs/archive/0.0.2.tar.gz"
   sha256 "6e1d314ef3898bc92b64aa2bffb1b24b7011b07914a0e95617227f32a1450547"
 
+  depends_on "findutils"
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
